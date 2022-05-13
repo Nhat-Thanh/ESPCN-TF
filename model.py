27cb0a2e11ee698953173c6977860576b95d8a0d
Nhat-Thanh <luonkebenban@gmail.com>
2022-05-13 09:18:08 +0700

    add save-log functionality

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
-from utils.common import exists
-import tensorflow as tf
+import os
 import neuralnet as nn
 import numpy as np
+from utils.common import exists
+import tensorflow as tf
 
-# -----------------------------------------------------------
-#  SRCNN
-# -----------------------------------------------------------
+class logger:
+    def __init__(self, path, values) -> None:
+        self.path = path
+        self.values = values
 
 class ESPCN:
     def __init__(self, scale=2):
@@ -26,34 +28,36 @@ class ESPCN:
         self.ckpt = None
         self.ckpt_dir = None
         self.ckpt_man = None
-    
+
     def setup(self, optimizer, loss, metric, model_path):
         self.optimizer = optimizer
         self.loss = loss
         self.metric = metric
         # @the best model weights
         self.model_path = model_path
-    
+
     def load_checkpoint(self, ckpt_dir):
         self.ckpt_dir = ckpt_dir
-        self.ckpt = tf.train.Checkpoint(step=tf.Variable(0), 
+        self.ckpt = tf.train.Checkpoint(step=tf.Variable(0),
                                         optimizer=self.optimizer,
                                         net=self.model)
-        self.ckpt_man = tf.train.CheckpointManager(self.ckpt, ckpt_dir, max_to_keep=1)
+        self.ckpt_man = tf.train.CheckpointManager(
+            self.ckpt, ckpt_dir, max_to_keep=1)
         self.ckpt.restore(self.ckpt_man.latest_checkpoint)
-    
+
     def load_weights(self, filepath):
         self.model.load_weights(filepath)
 
     def predict(self, lr):
         sr = self.model(lr)
         return sr
-    
-    def evaluate(self, dataset, batch_size=32):
+
+    def evaluate(self, dataset, batch_size=64):
         losses, metrics = [], []
         isEnd = False
         while isEnd == False:
-            lr, hr, isEnd = dataset.get_batch(batch_size, shuffle_each_epoch=False)
+            lr, hr, isEnd = dataset.get_batch(
+                batch_size, shuffle_each_epoch=False)
             sr = self.predict(lr)
             losses.append(self.loss(hr, sr))
             metrics.append(self.metric(hr, sr))
@@ -62,9 +66,21 @@ class ESPCN:
         loss = tf.reduce_mean(losses).numpy()
         return loss, metric
 
-    def train(self, train_set, valid_set, batch_size, 
-              steps, save_every=1, save_best_only=False):
-        
+    def train(self, train_set, valid_set, batch_size, steps, save_every=1,
+              save_best_only=False, save_log=False, log_dir=None):
+
+        if (save_log) and (log_dir is None):
+            ValueError("log_dir must be specified if save_log is True")
+        os.makedirs(log_dir, exist_ok=True)
+        dict_logger = {"loss":       logger(path=os.path.join(log_dir, "losses.npy"),      values=[]),
+                       "metric":     logger(path=os.path.join(log_dir, "metrics.npy"),     values=[]),
+                       "val_loss":   logger(path=os.path.join(log_dir, "val_losses.npy"),  values=[]),
+                       "val_metric": logger(path=os.path.join(log_dir, "val_metrics.npy"), values=[])}
+        for key in dict_logger.keys():
+            path = dict_logger[key].path
+            if exists(path):
+                dict_logger[key].values = np.load(path).tolist()
+
         cur_step = self.ckpt.step.numpy()
         max_steps = steps + self.ckpt.step.numpy()
 
@@ -74,39 +90,56 @@ class ESPCN:
             prev_loss, _ = self.evaluate(valid_set)
             self.load_checkpoint(self.ckpt_dir)
 
-        loss_mean = tf.keras.metrics.Mean()
-        metric_mean = tf.keras.metrics.Mean()
+        loss_buffer = []
+        metric_buffer = []
         while cur_step < max_steps:
             cur_step += 1
             self.ckpt.step.assign_add(1)
             lr, hr, _ = train_set.get_batch(batch_size)
             loss, metric = self.train_step(lr, hr)
-            loss_mean(loss)
-            metric_mean(metric)
+            loss_buffer.append(loss)
+            metric_buffer.append(metric)
 
             if (cur_step % save_every == 0) or (cur_step >= max_steps):
+                loss = tf.reduce_mean(loss_buffer).numpy()
+                metric = tf.reduce_mean(metric_buffer).numpy()
                 val_loss, val_metric = self.evaluate(valid_set)
                 print(f"Step {cur_step}/{max_steps}",
-                      f"- loss: {loss_mean.result():.7f}",
-                      f"- {self.metric.__name__}: {metric_mean.result():.3f}",
+                      f"- loss: {loss:.7f}",
+                      f"- {self.metric.__name__}: {metric:.3f}",
                       f"- val_loss: {val_loss:.7f}",
                       f"- val_{self.metric.__name__}: {val_metric:.3f}")
-                loss_mean.reset_states()
-                metric_mean.reset_states()
+
+                if save_log == True:
+                    dict_logger["loss"].values.append(loss)
+                    dict_logger["metric"].values.append(metric)
+                    dict_logger["val_loss"].values.append(val_loss)
+                    dict_logger["val_metric"].values.append(val_metric)
+
+                loss_buffer = []
+                metric_buffer = []
                 self.ckpt_man.save(checkpoint_number=0)
-                
+
                 if save_best_only and val_loss > prev_loss:
                     continue
                 prev_loss = val_loss
                 self.model.save_weights(self.model_path)
                 print(f"Save model to {self.model_path}\n")
 
-    @tf.function    
+        if save_log == True:
+            for key in dict_logger.keys():
+                logger_obj = dict_logger[key]
+                path = logger_obj.path
+                values = np.array(logger_obj.values, dtype=np.float32)
+                np.save(path, values)
+
+    @tf.function
     def train_step(self, lr, hr):
         with tf.GradientTape() as tape:
             sr = self.model(lr, training=True)
             loss = self.loss(hr, sr)
             metric = self.metric(hr, sr)
         gradient = tape.gradient(loss, self.model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradient, self.model.trainable_variables))
+        self.optimizer.apply_gradients(
+            zip(gradient, self.model.trainable_variables))
         return loss, metric
--- train.py
+++ train.py
@@ -7,22 +7,34 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--steps",          type=int, default=100000,          help='-')
-parser.add_argument("--batch-size",     type=int, default=128,             help='-')
-parser.add_argument("--scale",          type=int, default=2,               help='-')
-parser.add_argument("--save-every",     type=int, default=100,             help='-')
-parser.add_argument("--save-best-only", type=int, default=0,               help='-')
-parser.add_argument("--ckpt-dir",       type=str, default="checkpoint/x2", help='-')
+parser.add_argument("--steps",          type=int, default=100000, help='-')
+parser.add_argument("--scale",          type=int, default=2,      help='-')
+parser.add_argument("--batch-size",     type=int, default=128,    help='-')
+parser.add_argument("--save-every",     type=int, default=100,    help='-')
+parser.add_argument("--save-best-only", type=int, default=0,      help='-')
+parser.add_argument("--save-log",       type=int, default=0,      help='-')
+parser.add_argument("--ckpt-dir",       type=str, default="",     help='-')
 
+
+# -----------------------------------------------------------
+# global variables
+# -----------------------------------------------------------
 
 FLAG, unparsed = parser.parse_known_args()
 steps = FLAG.steps
 batch_size = FLAG.batch_size
-scale = FLAG.scale
-ckpt_dir = FLAG.ckpt_dir
 save_every = FLAG.save_every
-model_path = os.path.join(ckpt_dir, f"ESPCN-x{scale}.h5")
+save_log = (FLAG.save_log == 1)
 save_best_only = (FLAG.save_best_only == 1)
+
+scale = FLAG.scale
+if scale not in [2, 3, 4]:
+    ValueError("scale must be 2, 3 or 4")
+
+ckpt_dir = FLAG.ckpt_dir
+if (ckpt_dir == "") or (ckpt_dir == "default"):
+    ckpt_dir = f"checkpoint/x{scale}"
+model_path = os.path.join(ckpt_dir, f"ESPCN-x{scale}.h5")
 
 
 # -----------------------------------------------------------
@@ -49,15 +61,17 @@ valid_set.load_data()
 #  Train
 # -----------------------------------------------------------
 
-espcn = ESPCN(scale)
-espcn.setup(optimizer=Adam(learning_rate=2e-4),
-            loss=MeanSquaredError(),
-            model_path=model_path,
-            metric=PSNR)
+def main():
+    model = ESPCN(scale)
+    model.setup(optimizer=Adam(learning_rate=2e-4),
+                loss=MeanSquaredError(),
+                model_path=model_path,
+                metric=PSNR)
 
-espcn.load_checkpoint(ckpt_dir)
-espcn.train(train_set, valid_set, 
-            steps=steps, batch_size=batch_size,
-            save_best_only=save_best_only, 
-            save_every=save_every)
+    model.load_checkpoint(ckpt_dir)
+    model.train(train_set, valid_set, steps=steps, batch_size=batch_size,
+                save_best_only=save_best_only, save_every=save_every,
+                save_log=save_log, log_dir=ckpt_dir)
 
+if __name__ == "__main__":
+    main()
