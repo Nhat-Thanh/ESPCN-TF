87d9f61f5f18a890f65bc40ba6e0689315c5888c
Nhat-Thanh <luonkebenban@gmail.com>
2022-05-13 09:18:38 +0700

    add main functions and fix logic errors

--- demo.py
+++ demo.py
@@ -2,57 +2,55 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
 
 from utils.common import *
-import numpy as np
 from model import ESPCN
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--scale',        type=float, default=2,                           help='-')
-parser.add_argument("--image-path",   type=str,   default="dataset/test2.png",         help='-')
-parser.add_argument("--ckpt-path",    type=str,   default="checkpoint/x2/ESPCN-x2.h5", help='-')
+parser.add_argument('--scale',      type=int, default=2,                   help='-')
+parser.add_argument("--ckpt-path",  type=str, default="",                  help='-')
+parser.add_argument("--image-path", type=str, default="dataset/test1.png", help='-')
+
+
+# -----------------------------------------------------------
+# global variables
+# -----------------------------------------------------------
 
 FLAGS, unparsed = parser.parse_known_args()
-scale = FLAGS.scale
 image_path = FLAGS.image_path
-ckpt_path = FLAGS.ckpt_path
 
+scale = FLAGS.scale
 if scale not in [2, 3, 4]:
     ValueError("scale must be 2, 3, or 4")
 
-# -----------------------------------------------------------
-#  load model
-# -----------------------------------------------------------
+ckpt_path = FLAGS.ckpt_path
+if (ckpt_path == "") or (ckpt_path == "default"):
+    ckpt_path = f"checkpoint/x{scale}/ESPCN-x{scale}.h5"
 
-model = ESPCN(scale)
-model.load_weights(ckpt_path)
-
-
-# -----------------------------------------------------------
-#  read image and save bicubic image
-# -----------------------------------------------------------
-
-lr_image = read_image(image_path)
-bicubic_image = upscale(lr_image, scale)
-write_image("bicubic.png", bicubic_image)
+sigma = 0.3 if scale == 2 else 0.2
 
 
 # -----------------------------------------------------------
-# preprocess lr image 
-# -----------------------------------------------------------
-lr_image = gaussian_blur(lr_image, sigma=0.3)
-lr_image = rgb2ycbcr(lr_image)
-lr_image = norm01(lr_image[tf.newaxis, ...])
-
-
-# -----------------------------------------------------------
-#  predict and save image
+# demo
 # -----------------------------------------------------------
 
-sr_image = model.predict(lr_image)[0]
+def main():
+    lr_image = read_image(image_path)
+    bicubic_image = upscale(lr_image, scale)
+    write_image("bicubic.png", bicubic_image)
 
-sr_image = denorm01(sr_image)
-sr_image = np.uint8(sr_image)
-sr_image = ycbcr2rgb(sr_image)
+    lr_image = gaussian_blur(lr_image, sigma=sigma)
+    lr_image = rgb2ycbcr(lr_image)
+    lr_image = norm01(lr_image)
+    lr_image = tf.expand_dims(lr_image, axis=0)
 
-write_image("sr.png", sr_image)
+    model = ESPCN(scale)
+    model.load_weights(ckpt_path)
+    sr_image = model.predict(lr_image)[0]
 
+    sr_image = denorm01(sr_image)
+    sr_image = tf.cast(sr_image, tf.uint8)
+    sr_image = ycbcr2rgb(sr_image)
+    write_image("sr.png", sr_image)
+
+if __name__ == "__main__":
+    main()
