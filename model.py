fb983609e13d3cedd511a56cc79d75f45b3ad80a
Nhat-Thanh <luonkebenban@gmail.com>
2022-05-13 10:13:51 +0700

    fix ValueError raising

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ class ESPCN:
         elif scale == 4:
             self.model = nn.ESPCNx4()
         else:
-            ValueError("\"scale\" must be 2, 3, 4")
+            raise ValueError("\"scale\" must be 2, 3, 4")
 
         self.optimizer = None
         self.loss =  None
@@ -70,7 +70,7 @@ class ESPCN:
               save_best_only=False, save_log=False, log_dir=None):
 
         if (save_log) and (log_dir is None):
-            ValueError("log_dir must be specified if save_log is True")
+            raise ValueError("log_dir must be specified if save_log is True")
         os.makedirs(log_dir, exist_ok=True)
         dict_logger = {"loss":       logger(path=os.path.join(log_dir, "losses.npy"),      values=[]),
                        "metric":     logger(path=os.path.join(log_dir, "metrics.npy"),     values=[]),
